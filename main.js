d420134aba7502fdfdb78f8cff324ad44c4c33ac
NatanAlkmim <natan.alkmim@gmail.com>
2026-06-24 18:15:27 -0300

    Implement square clearing Click-to-color functionality Implement grid size

--- main.js
+++ main.js
@@ -1,47 +1,69 @@
 const container = document.querySelector("#container");
-const checkbox = document.querySelector("#mudar-cor");
+const gridSize = document.querySelector("#grid-size");
+const changeColor = document.querySelector("#mudar-cor");
 const clearGrid = document.querySelector("#clear-all");
+const clickToColor = document.querySelector("#click-to-color");
 
-function createGrid(container) {
-  for (let i = 0; i < 256; i++) {
+
+function userSize(gridSize) {
+  gridSize.addEventListener("mousedown", () => {
+
+    let getSizeGrid = prompt("Escolha um valor de 1 a 100");
+
+    while (getSizeGrid < 1 || getSizeGrid > 100) {
+            if (getSizeGrid === null) {
+        break;
+      }
+      alert("Precisa ser de 1 a 100!!!");
+      getSizeGrid = prompt("Escolha um valor de 1 a 100");
+    }
+    container.innerHTML = '';
+    createGrid(container, getSizeGrid);
+  })
+}
+
+userSize(gridSize)
+
+
+function createGrid(container, userSize) {
+  for (let i = 0; i < userSize ** 2; i++) {
     const div = document.createElement("div");
+    const hMin = 200;
+    const hMax = 280;
+    const h = Math.floor(Math.random() * (hMax - hMin) + hMin);
+    const hslColor = `hsl(${h}, 60%, 80%)`;
+    
+    div.style.width = `calc(100% / ${userSize})`;
+    div.style.height = `calc(100% / ${userSize})`;
+
     div.addEventListener("mouseenter", () => {
 
-      if (checkbox.checked) {
-        const hMin = 200;
-        const hMax = 280;
-        const h = Math.floor(Math.random() * (hMax - hMin) + hMin);
-        div.style.backgroundColor = `hsl(${h}, 60%, 80%)`;
-      } else {
+      if (changeColor.checked && !clickToColor.checked) {
+        div.style.backgroundColor = hslColor;
+      } else if (!clickToColor.checked) {
         div.style.backgroundColor = "gray";
       }
 
     });
+
+    div.addEventListener("mousedown", () => {
+      
+      if (clickToColor.checked && changeColor.checked) {
+        div.style.backgroundColor = hslColor;
+      } else if (clickToColor.checked){
+        div.style.backgroundColor = "gray";
+      }
+
+    });
+
     container.appendChild(div);
   }
 }
-createGrid(container);
+createGrid(container, 4);
 
 
-function clearcolors(clearGrid) {
-  clearGrid.addEventListener("click", () => {
-    container.innerHTML = '';
-    createGrid(container);
-  });
-}
-clearcolors(clearGrid);
-
-
-
-//container.innerHTML = '';
-
-//btnPedra.addEventListener("click", () => playRound("pedra", getComputerChoice()));
-
-/*
-const el = document.querySelector("#meu-elemento");
-
-el.addEventListener("mouseleave", () => {
-  el.style.backgroundColor = '';
-  el.style.color = '';
+clearGrid.addEventListener("click", () => {
+  container.innerHTML = '';
+  createGrid(container, userSize);
 });
-*/
+
