42ef1708a0c3407cb74ffed27fdadfb590c4ffc8
NatanAlkmim <natan.alkmim@gmail.com>
2026-06-24 18:53:15 -0300

    Fix startup without the grid

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const changeColor = document.querySelector("#mudar-cor");
 const clearGrid = document.querySelector("#clear-all");
 const clickToColor = document.querySelector("#click-to-color");
 
+let tamanhoAtual = 16;
 
 function userSize(gridSize) {
   gridSize.addEventListener("mousedown", () => {
@@ -11,13 +12,15 @@ function userSize(gridSize) {
     let getSizeGrid = prompt("Escolha um valor de 1 a 100");
 
     while (getSizeGrid < 1 || getSizeGrid > 100) {
-            if (getSizeGrid === null) {
+      if (getSizeGrid === null) {
         break;
       }
       alert("Precisa ser de 1 a 100!!!");
       getSizeGrid = prompt("Escolha um valor de 1 a 100");
     }
+
     container.innerHTML = '';
+    tamanhoAtual = getSizeGrid;
     createGrid(container, getSizeGrid);
   })
 }
@@ -28,16 +31,17 @@ userSize(gridSize)
 function createGrid(container, userSize) {
   for (let i = 0; i < userSize ** 2; i++) {
     const div = document.createElement("div");
-    const hMin = 200;
-    const hMax = 280;
-    const h = Math.floor(Math.random() * (hMax - hMin) + hMin);
-    const hslColor = `hsl(${h}, 60%, 80%)`;
     
     div.style.width = `calc(100% / ${userSize})`;
     div.style.height = `calc(100% / ${userSize})`;
 
     div.addEventListener("mouseenter", () => {
 
+      const hMin = 200;
+      const hMax = 280;
+      const h = Math.floor(Math.random() * (hMax - hMin) + hMin);
+      const hslColor = `hsl(${h}, 60%, 80%)`;
+      
       if (changeColor.checked && !clickToColor.checked) {
         div.style.backgroundColor = hslColor;
       } else if (!clickToColor.checked) {
@@ -59,11 +63,11 @@ function createGrid(container, userSize) {
     container.appendChild(div);
   }
 }
-createGrid(container, 4);
+createGrid(container, 16);
 
 
 clearGrid.addEventListener("click", () => {
   container.innerHTML = '';
-  createGrid(container, userSize);
+  createGrid(container, tamanhoAtual);
 });
 
